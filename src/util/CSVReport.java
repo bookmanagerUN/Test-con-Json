937535e38ad41683485a5892577bd116bd360caa
DESKTOP-R317JOJ\diego <adfasdfasdf>
2021-07-24 20:42:40 -0500

    Actualización de reporte CSV

--- src/util/CSVReport.java
+++ src/util/CSVReport.java
@@ -10,10 +10,8 @@ import Data.Notes;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.Writer;
 import util.treeStructures.AVLTree;
 
@@ -56,6 +54,7 @@ public class CSVReport {
         writer.write("Category:"+this.book.getBookInformation().getCategory() + "\n");
         writer.write("Opinion:"+this.book.getOpinion() + "\n");
         writer.write("\n");
+        writer.write("Page:Paragraph:Date:Note\n");
         this.notes.inorder(function);
         writer.flush();
         writer.close();
